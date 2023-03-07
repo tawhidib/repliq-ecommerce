ddd6c36bdf9678ec2bfec8738de9efa19230fcc9
tawhidib <tawhidib@gmail.com>
2023-03-07 07:52:12 +0600

    style: Basic navbar design added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,26 @@
+import { FaCartArrowDown } from "react-icons/fa";
+
 export default function Navbar(): JSX.Element {
-  return <></>;
+  return (
+    <div className="mx-auto block w-full max-w-screen-xl py-2 px-4 lg:px-8 lg:py-4">
+      <div className="flex items-center">
+        <div className="flex-none">
+          <h3>Repliq</h3>
+        </div>
+        <div className="flex-auto">
+          <ul className="hidden items-center gap-6 lg:flex justify-center">
+            <li>Home</li>
+            <li>Dashboard</li>
+            <li>Products</li>
+            <li>Orders</li>
+          </ul>
+        </div>
+        <div className="flex-none">
+          <button className="middle none center rounded-lg bg-pink-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
+            <FaCartArrowDown />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import { Inter } from "next/font/google";
-import NavbarComponent from "../components/Navbar";
+import Navbar from "../components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,7 +14,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <NavbarComponent />
+        <Navbar />
       </main>
     </>
   );
