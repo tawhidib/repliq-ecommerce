cc175a3d8386459cfd2bfa0295b59faea2e8da11
tawhidib <tawhidib@gmail.com>
2023-03-08 07:48:53 +0600

    pref:(Produts/index.tsx): ProductType created
    Product type created for type safety

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,3 +1,19 @@
+import { useEffect, useState } from "react";
+
+export type ProductType = {
+  id: number;
+  title: string;
+  description: string;
+  category: string;
+  image: string;
+  rating: {
+    rate: number;
+    count: number;
+  };
+};
+
 export default function ProductsPage(): JSX.Element {
+  const [products, setProducts] = useState<ProductType[]>([]);
+
   return <></>;
 }
